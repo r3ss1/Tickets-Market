d44abfb0b358a95e04e7c475c03e6b2458877cf8
aessary <raressilviulazar@gmail.com>
2025-01-10 13:11:02 +0000

    Header P1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ConvexClientProvider>
         <ClerkProvider>
+          <Header />
         {children}
         </ClerkProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
